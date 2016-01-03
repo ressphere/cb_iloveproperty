77b361be0f898ff21f3c2052234325ad1bfa8ccd
ckong <ckong@ck>
2016-01-03 16:32:37 +0800

    Read from USERS db to limit user's listing

--- trunk/circle_property/fuel/application/controllers/CBWS_AUTH_Service/CBWS_Member/CBWS_Member.php
+++ trunk/circle_property/fuel/application/controllers/CBWS_AUTH_Service/CBWS_Member/CBWS_Member.php
@@ -22,6 +22,13 @@ class CBWS_Member {
         }
         return $countries;
     }
+    
+    public function get_user_property_listing_limit($user_id)
+    {
+
+        return $this->CI->tank_auth->get_user_prop_listing_limit($user_id);
+    }
+    
     public function create_user($username, $display_name, $email, $password, $phone, $email_activation, $country)
     {
         //return "Create User";
