5ce557df03fe19d09bb26cd110d4a8de03409d1e
user <user@192.168.182.1>
2016-01-31 15:21:48 +0800

    BG-13 [Property][Preview] Cannot Preview page and page is not showing correctly

--- trunk/client_views/properties_views/js/property_preview.js
+++ trunk/client_views/properties_views/js/property_preview.js
@@ -104,10 +104,11 @@ var set_facilities = function($scope, facilities)
 {
   var count = 0;
   $scope.property_information.property_facilities = [[],[],[]];
+  console.log($scope.property_information.property_facilities);
   for(var i = 0; i< facilities.length; i++)
   {
       $scope.property_information.property_facilities[count].push(facilities[i]);
-      if(++count > 3)
+      if(++count >= 3)
       {
           count = 0;
       }
