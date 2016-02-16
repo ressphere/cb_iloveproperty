704f324de4c4d72a20253e11c39f57f7291d5613
user <user@192.168.182.1>
2016-02-16 23:30:34 +0800

    change the contact_us id to contact

--- trunk/client_views/properties_views/application/views/_usercontrols/cb_properties_category_doughnut.php
+++ trunk/client_views/properties_views/application/views/_usercontrols/cb_properties_category_doughnut.php
@@ -1,4 +1,4 @@
-<img id="property_title" src="images/middle_image.png" style="display: none" >
+<img id="property_title" src="images/welcome.png" style="display: none">
 <center><canvas id="property_service_category" height=<?=$width?> width=<?=$height?>></canvas></center>
 <center><img id="property_shadow" src="images/shadow.png" width=<?php echo ($width / 2.0)?>></center>
 <div id="log"></div>
