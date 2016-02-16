8fe667a13d337145161d61e476a24044aa7c45ef
user <user@192.168.182.1>
2016-02-17 00:35:21 +0800

    control the maxlength to 50 characters

--- trunk/client_views/properties_views/application/views/_usercontrols/cb_edit_listing_info.php
+++ trunk/client_views/properties_views/application/views/_usercontrols/cb_edit_listing_info.php
@@ -31,7 +31,7 @@
                      </div>
                      <div>
                          <center>
-                             <textarea readonly maxlength="200" class="form-control photo-desc" style='resize: none;' cols='35' rows='5'>{{uploaded_file[1]}}</textarea>
+                             <textarea readonly maxlength="200" class="form-control photo-desc" maxlength="50" style='resize: none;' cols='35' rows='5'>{{uploaded_file[1]}}</textarea>
                          </center>
                      </div>
                 </div>
