8ad8ffd16d57512aa1ab547f2a3bb5254955bf7d
ckong <ckong@ck>
2016-03-13 15:37:26 +0800

    BG101 - [Ressphere] User registration terms &amp; conditions

---
+++ trunk/circle_property/fuel/application/views/_usercontrols/policy_content.php
@@ -0,0 +1,36 @@
+<div class="unfixed_content row clearfix container-fluid">
+       
+       <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Term and Conditions</h2>
+                </div>
+       </div>
+       <div class="row">
+                <div class="col-md-12 column">
+                    <?php
+
+                        echo '<div class="about_us" style="height:100%" >
+                              
+                              TERMS OF USE
+
+1. ACCEPTANCE OF TERMS
+
+Welcome to www.iproperty.com.my. Iproperty.com Malaysia Sdn Bhd ("we" and "us", as the case may be) provides the Services including the use of this website(as defined below) to you, subject to the following Terms of Use, which may be updated by us in our sole discretion from time to time with or without notice to you. You are deemed to have been apprised of and bound by any changes to the Terms of Use. You agree that your continued use of the Service after such update will constitute your acceptance of and agreement to be bound by the updated Terms of Use. [You can review the most current version of the Terms of Use at any time at www.iproperty.com.my/policy/term.htm]. In addition, when using the Services or the website, you shall be subject to any posted guidelines or rules applicable to such services, which may be posted from time to time. All such guidelines or rules are hereby incorporated by reference into the Terms of Use. In most cases the guidelines and rules are specific to a particular part of the Service and will assist you in applying the Terms of Use to that part, but to the extent of any inconsistency between the Terms of Use and any guideline or rule, the Terms of Use will prevail.
+
+2. DESCRIPTION OF SERVICE
+
+We provide users with access and use of materials or Contents on http://www.iproperty.com.my (the "Service") through our network of properties which may be accessed through any various medium or device now known or hereafter developed, including but not limited to the World Wide Web. You also understand and agree that the Service may include certain communications from us, such as service announcements, administrative messages and newsletters. Unless explicitly stated otherwise, any new features that augment or enhance the current Service shall be subject to the Terms of Use. You understand and agree that the Service is provided on an "AS-IS" basis and that we assume no responsibility for the timeliness, deletion, mis-delivery or failure to store any user communications or personalisation settings. You are responsible for obtaining access to the Service, and that access may involve third-party fees (such as Internet service provider or airtime charges). You are responsible for those fees, including those fees associated with the display or delivery of advertisements. In addition, you must provide and are responsible for all equipment necessary to access the Service. Unless expressly stated otherwise, no information presented in the Service or in connection with any products and services forming part of the Service shall be deemed as a binding offer by us or the relevant third party, but may constitute an invitation for you to place an order. In respect of contracts for our or any third party products and services which are made available as part of the Service, these shall be deemed concluded when we or the relevant third party have accepted your order for the same or have provided you with the product or service pursuant to your order.
+
+3. YOUR REGISTRATION OBLIGATIONS
+
+In consideration of your use of the Service, you represent that you are of legal age to form a binding contract and are not a person barred from receiving services under the laws of Malaysia or other applicable jurisdiction. You also agree to: (a) provide true, accurate, current and complete information about yourself and (b) maintain and promptly update the Registration Data to keep it true, accurate, current and complete. If you provide any information that is untrue, inaccurate, not current or incomplete, or we have reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, we reserve the right to suspend or terminate your account and refuse any and all current or future use of the Service (or any portion thereof). We may provide you with access to some parts of the Service without you registering as a user.
+
+4. PRIVACY  
+
+
+                              </div>';
+                    ?>
+                </div>
+       </div>
+</div>
+
