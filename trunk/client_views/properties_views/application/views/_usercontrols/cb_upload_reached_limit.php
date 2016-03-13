700c7a929cd6f057f8898ccd39bd4e39a1d2bf9f
user <user@192.168.182.1>
2016-03-13 16:04:58 +0800

    user profile update with url

--- trunk/client_views/properties_views/application/views/_usercontrols/cb_upload_reached_limit.php
+++ trunk/client_views/properties_views/application/views/_usercontrols/cb_upload_reached_limit.php
@@ -4,6 +4,6 @@
         <!--
         <button type="button" class="btn btn-primary" style="border-radius: 5px">Login</button>
         -->
-        <button type="button" class="btn btn-primary" onclick="window.location.href ='http://www.ressphere.com/index.php/cb_user_profile_update/my_profile'" style="border-radius: 5px">Profile</button>
+        <button id="btn_profile" type="button" class="btn btn-primary" style="border-radius: 5px">Profile</button>
     </p>
 </center>
--- trunk/client_views/properties_views/js/property_upload_reached_limit.js
+++ trunk/client_views/properties_views/js/property_upload_reached_limit.js
@@ -1,5 +1,47 @@
+var get_upload_reached_limit_home = function() {
+        return {
+            Extends: get_base(),
+            Initialize: function( private ){       
+                this.parent.Initialize();
+            },
+            Private:{
+                
+            },
+            Public:{         
+            }
+        };
+ };
+
+var StaticUploadReachedLimitObject = (function () {
+        var objUploadReachedLimit;
+
+        function createHomeInstance() {
+            var objUploadReachedLimit = $.makeclass(get_upload_reached_limit_home());
+            return objUploadReachedLimit;
+        }
+
+        return {
+            getInstance: function () {
+                if (!objUploadReachedLimit) {
+                    objUploadReachedLimit = createHomeInstance();
+                }
+                return objUploadReachedLimit;
+            }
+        };
+})();
+
+
+
 $(document).ready(function()
 {
+   var uploadListing = StaticUploadReachedLimitObject.getInstance();
+   $('#btn_profile').click(function()
+       {
+           var wsdl = uploadListing.getWsdlBaseUrl() + 'index.php/cb_user_profile_update/my_profile#Property_Profile';
+           
+           window.location.href = wsdl;
+       }
+    );
    $("#btnBack").click(
       function()
       {
