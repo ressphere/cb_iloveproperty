0e3f864f21e937f57a87f3c8e4285a1d01f3d7ae
user <user@192.168.182.1>
2016-04-25 08:37:12 +0800

    add link for google navigation

--- trunk/client_views/properties_views/js/property_details_page.js
+++ trunk/client_views/properties_views/js/property_details_page.js
@@ -6,7 +6,7 @@
 // <editor-fold desc="set google nearby place radius and type(special for property)"  defaultstate="collapsed">
 
 ng_map_profile.config(function(ngGPlacesAPIProvider, $compileProvider) {
-            $compileProvider.aHrefSanitizationWhitelist(/^\s*(geo|https?|ftp|mailto|chrome-extension):/)
+            $compileProvider.aHrefSanitizationWhitelist(/^\s*(geo|https?|ftp|mailto|chrome-extension):/);
             ngGPlacesAPIProvider.setDefaults({
                 radius: 1000,
                 types: ['bank', 'school', 'shopping_mall', 'hospital', 'airport', 'subway_station', 'bus_station', 'train_station',
