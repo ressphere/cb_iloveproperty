3e51d8f4ccf6bef4aac3eb7e4b9d60ecc18eae94
user <user@192.168.182.1>
2017-03-02 23:32:55 +0800

    revert sms

--- trunk/circle_property/db/cb_db/s2_cb_user.sql
+++ trunk/circle_property/db/cb_db/s2_cb_user.sql
@@ -94,6 +94,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `agent`   TINYINT(1) NOT NULL DEFAULT '0',
   `country_id` int COLLATE utf8_bin NOT NULL DEFAULT '1',
   `prop_listing_limit` int COLLATE utf8_bin DEFAULT '3',
+  `prop_sms_limit` int COLLATE utf8_bin DEFAULT '0'
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`country_id`) REFERENCES country(`id`)
